824cb894f5379c61d1e3b7b6404fa040d615f080
jxwdsb <1781642940@qq.com>
2018-07-13 12:51:54 +0800

    aria2 webui 放到站点

---
+++ aria2ui.sh
@@ -0,0 +1,23 @@
+#
+echo ¨html: aria2-WebUi¨
+read -t 30 -n 1 -p "开始布置吗?[y/n]:" start 
+if [[ $start == "y" ]]; then
+	echo
+	echo -e "\033[32m 开始布置 \033[0m"
+	#开始执行
+	cd /var/xinwen/www
+	wget https://codeload.github.com/ziahamza/webui-aria2/zip/master  
+	7z x webui-aria2-master.zip -r -o/var/xinwen/www -y 
+	mv ./webui-aria2-master ./aria2ui 
+	chmod -R 755 ./aria2ui 
+	rm webui-aria2-master.zip
+	echo -e "\033[32m 布置完成 \033[0m"
+	echo -e "\033[32m nginx 站点:https://yourip/aria2ui \033[0m"
+	echo -e "\033[32m nginx 作者:Github.com ziahamza/webui-aria2 \033[0m"
+	cd /root
+	exit 0
+else
+	echo 
+	echo -e  "\033[31m 取消布置 \033[0m"
+	exit 0
+fi 
