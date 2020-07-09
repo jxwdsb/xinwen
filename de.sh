fd078bcea03d64db94f678bbb6d9f2a63348e3e9
jxwdsb <1781642940@qq.com>
2020-07-09 10:51:12 +0800

    Debian 一键

--- de.sh
+++ de.sh
@@ -17,37 +17,45 @@ curl -sSL https://get.docker.com/ | sh
 docker run -d --name  speedtest -p 0.0.0.0:8099:80 adolfintel/speedtest:latest
 apt-get install p7zip-full -y
 
-7z x xinwen.7z -r -o/var -y
-chmod -R 755 ./xinwen
-rm xinwen.7z 
-
 mkdir /var/xinwen
 chmod -R 755 /var/xinwen
 cd /var/xinwen
 mkdir 123 nginx www
 chmod -R 755 123 nginx www
-echo -e 'FROM php:fpm\nRUN apt-get update && docker-php-ext-install mysqli && docker-php-ext-install pdo_mysql && docker-php-ext-install pcntl ' > /var/xinwen/123/Dockerfile1
+echo -e 'FROM php:fpm\nRUN apt-get update && docker-php-ext-install mysqli && docker-php-ext-install pdo_mysql && docker-php-ext-install pcntl ' > /var/xinwen/123/Dockerfile
+
 
 docker pull mariadb 
 docker run --name mymariadb -d -e MYSQL_ROOT_PASSWORD=$passwd mariadb 
 
-docker pull php:fpm
-cd /var/xinwen/123 
-docker build -t myphp .
-docker run --name myphp -d -p 8281:8281 -p 8280:8280 -p 8282:8282 -v /var/xinwen/www:/var/www/html1 --link mymariadb:mysql myphp 
-docker exec -i -t myphp /bin/bash -c 'mv /var/www/html/* /var/www/html1'
-docker exec -i -t myphp /bin/bash -c 'rm -rf /var/www/html'
-docker exec -i -t myphp /bin/bash -c 'ln -s /var/www/html1 /var/www/html'
-docker restart myphp
 
 docker pull nginx 
-docker run --name mynginx -d -p 80:80 -p 443:443 -v /var/xinwen/www:/usr/share/nginx/html1 -v /var/xinwen/nginx:/etc/nginx/conf.d1 --link myphp:php nginx
+docker run --name mynginx -d -p 80:80 -p 443:443 -v /var/xinwen/www:/usr/share/nginx/html1 -v /var/xinwen/nginx:/etc/nginx/conf.d1 nginx
 docker exec -i -t mynginx /bin/bash -c 'mv /etc/nginx/conf.d/* /etc/nginx/conf.d1'
 docker exec -i -t mynginx /bin/bash -c 'rm -rf /etc/nginx/conf.d'
 docker exec -i -t mynginx /bin/bash -c 'ln -s /etc/nginx/conf.d1 /etc/nginx/conf.d'
+docker exec -i -t mynginx /bin/bash -c 'mv /usr/share/nginx/html/* /usr/share/nginx/html1'
+docker exec -i -t mynginx /bin/bash -c 'rm -rf /usr/share/nginx/html'
+docker exec -i -t mynginx /bin/bash -c 'ln -s /usr/share/nginx/html1 /usr/share/nginx/html'
 docker restart mynginx
+
+
+docker pull php:fpm
+cd /var/xinwen/123 
+docker build -t myphp .
+docker run --name myphp -d -p 8281:8281 -p 8280:8280 -p 8282:8282 -v /var/xinwen/www:/var/www/html --link mymariadb:mysql myphp 
+
+mv /var/xinwen/nginx/default.conf /var/xinwen/nginx/default.conf.backup
+wget -P /var/xinwen/nginx https://raw.githubusercontent.com/jxwdsb/xinwen/master/default.conf
+docker restart mynginx
+
+cd /var/xinwen/www
+wget https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.zip
+7z x phpMyAdmin-latest-all-languages.zip -r -o/var/xinwen/www -y
+mv phpMyAdmin-5.0.2-all-languages phpmyadmin
 
 ln -s /var/xinwen /root/xinwen
 cd /root
 
-
+#docker stop myphp && docker rm myphp
+#docker stop mynginx && docker rm mynginx
