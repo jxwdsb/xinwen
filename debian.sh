7a65457a923d035a2a5b3e7fe6b4ccc86f7453a3
jxwdsb <1781642940@qq.com>
2020-08-22 15:23:09 +0800

    Add files via upload

--- debian.sh
+++ debian.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#¨在Debian上安装Docker 快速搭建网站环境¨
+#¨在 debian 上安装 Docker 快速搭建网站环境¨
 #¨v: 0.0.2¨
 #¨By: xinwen¨
-echo ¨debian [docker: mymariadb php-fpm nginx]¨
+echo ¨ [docker: mymariadb php-fpm nginx]¨
 read -s -p "设置密码:" passwd 
 if [[ ! -n "$passwd" ]]; then
 	echo -e "\033[31m 安装错误.未设置密码 \033[0m"
@@ -10,6 +10,7 @@ if [[ ! -n "$passwd" ]]; then
 fi
 echo
 echo -e "\033[32m 开始安装 \033[0m"
+
 apt-get update -y
 apt-get install wget -y #防止是复制粘贴的代码
 apt-get install curl -y 
@@ -17,18 +18,30 @@ curl -sSL https://get.docker.com/ | sh
 #docker run -d --name  speedtest -p 0.0.0.0:8099:80 adolfintel/speedtest:latest
 apt-get install p7zip-full -y
 
+systemctl start docker
+systemctl stop firewalld
+
+#yum http://rpmfind.net/linux/mageia/distrib/cauldron/x86_64/media/core/release/p7zip-16.02-7.mga8.x86_64.rpm
+#wget https://raw.githubusercontent.com/jxwdsb/xinwen/master/p7zip-16.02-7.mga8.x86_64.rpm
+#yum install -y p7zip*.rpm
+
+#docker run -d -p 6688:80 ilemonrain/html5-speedtest:alpine
+docker pull mariadb 
+docker pull php:fpm
+docker pull nginx 
+
+
 mkdir /var/xinwen
 chmod -R 755 /var/xinwen
 cd /var/xinwen
 mkdir 123 nginx www php
 chmod -R 755 123 nginx www php
-echo -e 'FROM php:fpm\nRUN apt-get update && docker-php-ext-install mysqli && docker-php-ext-install pdo_mysql && docker-php-ext-install pcntl ' > /var/xinwen/123/Dockerfile
+echo -e 'FROM php:fpm\nRUN apt-get update && docker-php-ext-install mysqli && docker-php-ext-install pdo_mysql && docker-php-ext-install pcntl && apt-get install -y  libfreetype6-dev  libjpeg62-turbo-dev  libpng-dev && docker-php-ext-configure gd --with-freetype --with-jpeg && docker-php-ext-install -j$(nproc) gd && apt-get install -y zlib1g-dev libzip-dev && docker-php-ext-install zip' > /var/xinwen/123/Dockerfile
 
-
-docker pull mariadb 
 docker run --name mymariadb -d -e MYSQL_ROOT_PASSWORD=$passwd mariadb 
+#docker restart mymariadb
+#docker exec -it  mymariadb mysqldump -uroot -proot glxt1 > /var/xinwen/www/test_db.sql
 
-docker pull php:fpm
 cd /var/xinwen/123 
 docker build -t myphp .
 #docker run --name myphp -d -p 8281:8281 -p 8280:8280 -p 8282:8282 -v /var/xinwen/www:/var/www/html -v /var/xinwen/php:/usr/local/etc/php1 --link mymariadb:mysql myphp 
@@ -36,9 +49,11 @@ docker run --name myphp -d -p 8281:8281 -p 8280:8280 -p 8282:8282 -v /var/xinwen
 docker exec -i -t myphp /bin/bash -c 'mv /usr/local/etc/php/* /usr/local/etc/php1'
 docker exec -i -t myphp /bin/bash -c 'rm -rf /usr/local/etc/php'
 docker exec -i -t myphp /bin/bash -c 'ln -s /usr/local/etc/php1 /usr/local/etc/php'
+docker exec -i -t myphp /bin/bash -c 'apt-get install p7zip-full -y'
+wget -P /var/xinwen/php/conf.d https://raw.githubusercontent.com/jxwdsb/xinwen/master/1.ini
 docker restart myphp
 
-docker pull nginx 
+#--network host
 #docker run --name mynginx -d -p 80:80 -p 443:443 -v /var/xinwen/www:/usr/share/nginx/html1 -v /var/xinwen/nginx:/etc/nginx/conf.d1 --link myphp:php nginx
 docker run --name mynginx -d -p 80:80 -p 443:443 -v /var/xinwen/www:/usr/share/nginx/html1 -v /var/xinwen/nginx:/etc/nginx/conf.d1 --privileged=true --link myphp:php nginx
 docker exec -i -t mynginx /bin/bash -c 'mv /etc/nginx/conf.d/* /etc/nginx/conf.d1'
@@ -55,7 +70,9 @@ docker restart mynginx
 
 cd /var/xinwen/www
 wget https://www.phpmyadmin.net/downloads/phpMyAdmin-latest-all-languages.zip
-7z x phpMyAdmin-latest-all-languages.zip -r -o/var/xinwen/www -y
+#7za x phpMyAdmin-latest-all-languages.zip -r -o/var/xinwen/www -y
+#docker exec -i -t myphp /bin/bash -c '7z x /var/www/html/assets.7z && chmod -R 755 assets '
+docker exec -i -t myphp /bin/bash -c '7z x phpMyAdmin-latest-all-languages.zip -r -y'
 mv phpMyAdmin-5.0.2-all-languages phpmyadmin
 wget -P /var/xinwen/www/phpmyadmin https://raw.githubusercontent.com/jxwdsb/xinwen/master/config.inc.php
 chmod -R 755 phpmyadmin
@@ -63,15 +80,3 @@ chmod -R 755 phpmyadmin
 
 ln -s /var/xinwen /root/xinwen
 cd /root
-
-#docker stop mynginx && docker rm mynginx
-#docker stop myphp && docker rm myphp
-#docker stop mymariadb && docker rm mymariadb
-#rm -rf /var/xinwen
-#rm -f /root/test.sh
-
-#然后容器里面通过 nsenter --mount=/host/proc/1/ns/mnt sh -c "ls /root" 这样执行。
-#如果需要网络数据的话用 nsenter --net=/host/proc/1/ns/net sh ...
-#nsenter --mount=/host/proc/1/ns/mnt sh -c "ls /root"
-#nsenter --mount=/host/proc/1/ns/mnt sh -c "docker ps -a"
-#nsenter --mount=/host/proc/1/ns/mnt sh -c "docker restart mynginx"
