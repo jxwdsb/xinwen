d15c516ef687b99134c2106278b90d4859b3e742
root <1781642940@qq.com>
2023-02-02 06:35:23 +0800

    update robot 1675290923

---
+++ http_service_files/app/controller/TestController.php
@@ -0,0 +1,19 @@
+<?php
+	
+namespace app\controller;
+
+use support\Request;
+
+
+class TestController
+{
+	function __construct()
+	{
+		//
+	}
+
+	public function index(Request $request)
+	{
+		return json(['code' => 0, 'msg' => 'ok']);
+	}
+}
