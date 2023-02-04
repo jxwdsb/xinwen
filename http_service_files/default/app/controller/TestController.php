1ad89d9ea681db81df23bc35badfc62f69a80591
root <1781642940@qq.com>
2023-02-05 01:09:14 +0800

    update robot 1675530554

--- http_service_files/default/app/controller/TestController.php
+++ http_service_files/default/app/controller/TestController.php
@@ -17,6 +17,17 @@ class TestController
 		return json(['code' => 0, 'msg' => 'ok']);
 	}
 
+	public function test(Request $request)
+	{
+		$data = Medoo::instance('test')->select('user', [
+			'id',
+			'token',
+		], [
+			'id' => 1,
+		]);
+		return json(['code' => 0, 'msg' => $data]);
+	}
+
 	public function sms_send(Request $request)
 	{
 		return json(['code' => 0, 'msg' => 'ok']);
