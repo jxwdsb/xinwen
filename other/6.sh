b091e19c4f941f4ae30713648677aa7ab43505be
jxwdsb <1781642940@qq.com>
2023-02-04 11:35:20 +0800

    Add files via upload

--- other/6.sh
+++ other/6.sh
@@ -4,7 +4,7 @@ die() {
 	local cmd=$1
 	local errorC=$2
 	if [[ errorC -gt 1 ]]; then
-		echo -e  "\033[31m错误太多次 $cmd \033[0m"
+		echo -e "\033[31m错误太多次 $cmd \033[0m"
 		exit;
 	fi
 	errorC=$(($errorC+1))
@@ -21,13 +21,14 @@ fi
 #大于0 就是找到了
 linux_version=`uname -v`
 if [[ `awk -v a="$linux_version" -v b="Debian" 'BEGIN{print index(a,b)}'` -le 0 ]]; then
-	echo -e  "\033[31m 不支持的系统 目前仅支持debian 系统 \033[0m"
+	echo -e "\033[31m 不支持的系统 目前仅支持debian 系统 \033[0m"
 	exit;
 fi
 
 echo "1.HOME 设置"
 echo "2.同步文件"
 echo "3.同步文件 并获取最新"
+
 read -n1 -p "请选择:" answer
 case $answer in
 	A | a | 1) echo
@@ -36,77 +37,116 @@ case $answer in
 		echo -e "\033[32mcontinue \033[0m";;
 	C | c | 3) echo
 		echo -e "\033[32mcontinue \033[0m";;
-	*) echo
-		echo -e  "\033[31mgoodbye \033[0m"
+	*)
+		echo -e "\033[31mgoodbye \033[0m"
 		exit;;
 esac
 
 cd /root
-apt update
-apt upgrade -y
+apt update >> /dev/null 2>&1
+apt upgrade -y >> /dev/null 2>&1
 
 errorC=0
 while [[ `type -t curl` == "" ]]; do
 	if [[ errorC -gt 1 ]]; then
-		echo -e  "\033[31m 安装 curl 错误太多次 \033[0m"
+		echo -e "\033[31m 安装 curl 错误太多次 \033[0m"
 		exit;
 	fi
-	cmd="apt install -y curl wget"
-	eval ${cmd} || die "$cmd"
 	errorC=$(($errorC+1))
+	cmd="apt -y install curl wget >> /dev/null 2>&1"
+	eval ${cmd} || die "$cmd"
 done
 
 function init() {
-	cmd="apt -y install redis-server mariadb-server"
-	eval ${cmd} || die "$cmd"
 
-	mysqladmin -uroot -proot password "root"
-	FIND_FILE="/etc/mysql/mariadb.conf.d/50-server.cnf"
-	FIND_STR="max_connections="
-	if [ `grep -c "$FIND_STR" $FIND_FILE` -ne '0' ];then
-		echo "跳过设置"
-		#exit 0
-	else
-		echo -e "\n\nmax_connections=3000" >> /etc/mysql/mariadb.conf.d/50-server.cnf
-	fi
-	systemctl restart mariadb
+	errorC=0
+	while [[ `type -t redis mariadb` == "" ]]; do
+		if [[ errorC -gt 1 ]]; then
+			echo -e "\033[31m 安装 redis mariadb 错误太多次 \033[0m"
+			exit;
+		fi
+		errorC=$(($errorC+1))
+		echo -e "\033[32mwait install redis-server mariadb-server \033[0m";
+		cmd="apt -y install redis-server mariadb-server >> /dev/null 2>&1"
+		eval ${cmd} || die "$cmd"
 
-	curl -sSL https://packages.sury.org/php/README.txt | bash -x
-	apt-cache showpkg php
+		mysqladmin -uroot -proot password "root"
+		FIND_FILE="/etc/mysql/mariadb.conf.d/50-server.cnf"
+		FIND_STR="max_connections="
+		if [ `grep -c "$FIND_STR" $FIND_FILE` -ne '0' ];then
+			echo "跳过设置"
+			#exit 0
+		else
+			echo -e "\n\nmax_connections=3000" >> /etc/mysql/mariadb.conf.d/50-server.cnf
+		fi
+		systemctl restart mariadb
+	done
 
-	cmd="apt -y install php8.0-cli php8.0-curl php8.0-mysql php8.0-pgsql php8.0-mbstring php8.0-imagick php8.0-gd php8.0-xml php8.0-zip"
-	eval ${cmd} || die "$cmd"
+	errorC=0
+	while [[ `type -t php` == "" ]]; do
+		if [[ errorC -gt 1 ]]; then
+			echo -e "\033[31m 安装 php 错误太多次 \033[0m"
+			exit;
+		fi
+		errorC=$(($errorC+1))
+		echo -e "\033[32mwait install php \033[0m";
+		curl -sSL https://packages.sury.org/php/README.txt | bash -x
+		apt-cache showpkg php
 
-	cmd="apt -y install git"
-	eval ${cmd} || die "$cmd"
+		cmd="apt -y install php8.0-cli php8.0-curl php8.0-mysql php8.0-pgsql php8.0-mbstring php8.0-imagick php8.0-gd php8.0-xml php8.0-zip >> /dev/null 2>&1"
+		eval ${cmd} || die "$cmd"
+	done
+
+	errorC=0
+	while [[ `type -t git` == "" ]]; do
+		if [[ errorC -gt 1 ]]; then
+			echo -e "\033[31m 安装 git 错误太多次 \033[0m"
+			exit;
+		fi
+		errorC=$(($errorC+1))
+		echo -e "\033[32mwait install git \033[0m";
+		cmd="apt -y install git >> /dev/null 2>&1"
+		eval ${cmd} || die "$cmd"
+	done
 	
 	cd /root
-	git clone https://github.com/jxwdsb/xinwen.git
+	echo -e "\033[32mwait git clone \033[0m";
+	git clone https://github.com/jxwdsb/xinwen.git >> /dev/null 2>&1
 	mv xinwen GitFiles
 
-	cmd="apt -y install screen"
-	eval ${cmd} || die "$cmd"
+	errorC=0
+	while [[ `type -t screen` == "" ]]; do
+		if [[ errorC -gt 1 ]]; then
+			echo -e "\033[31m 安装 screen 错误太多次 \033[0m"
+			exit;
+		fi
+		errorC=$(($errorC+1))
+		echo -e "\033[32mwait install screen \033[0m";
+		cmd="apt -y install screen >> /dev/null 2>&1"
+		eval ${cmd} || die "$cmd"
+	done
 }
 
 case $answer in
-	A | a | 1) echo
+	A | a | 1)
 
 		ip=`ip a|grep inet|grep brd|grep -v eth0:|grep -v 127.0.0.1|grep -v inet6|grep -v docker|awk '{print $2}'|awk -F'[/]' '{print $1}'|awk -F'[\n]' '{print $1}'` && echo $ip
 
 		errorC=0
 		while [[ `type -t nginx` == "" ]]; do
 			if [[ errorC -gt 1 ]]; then
-				echo -e  "\033[31m 安装 nginx 错误太多次 \033[0m"
+				echo -e "\033[31m 安装 nginx 错误太多次 \033[0m"
 				exit;
 			fi
 			errorC=$(($errorC+1))
+			echo -e "\033[32mwait install nginx \033[0m";
 
 			curl -sSL https://packages.sury.org/nginx/README.txt | bash -x
 			
-			cmd="apt -y install nginx"
+			cmd="apt -y install nginx >> /dev/null 2>&1"
 			eval ${cmd} || die "$cmd"
 			
-			cmd="apt -y install lua5.4 liblua5.4-dev luajit libnginx-mod-http-lua"
+			cmd="apt -y install lua5.4 liblua5.4-dev luajit libnginx-mod-http-lua >> /dev/null 2>&1"
 			eval ${cmd} || die "$cmd"
 
 			domainName=$ip
@@ -124,7 +164,7 @@ case $answer in
 		errorC=0
 		while [[ `type -t docker` == "" ]]; do
 			if [[ errorC -gt 1 ]]; then
-				echo -e  "\033[31m 安装 docker 错误太多次 \033[0m"
+				echo -e "\033[31m 安装 docker 错误太多次 \033[0m"
 				exit;
 			fi
 			errorC=$(($errorC+1))
@@ -135,22 +175,22 @@ case $answer in
 		errorC=0
 		while [[ `docker inspect hoppscotch 2> /dev/null` == "[]" ]]; do
 			if [[ errorC -gt 1 ]]; then
-				echo -e  "\033[31m docker hoppscotch 错误太多次 \033[0m"
+				echo -e "\033[31m docker hoppscotch 错误太多次 \033[0m"
 				exit;
 			fi
 			errorC=$(($errorC+1))
-
+			echo -e "\033[32mwait docker hoppscotch \033[0m";
 			docker run --rm --name hoppscotch -d -p 3000:3000 hoppscotch/hoppscotch:latest
 		done
 
 		errorC=0
 		while [[ `docker inspect aria2-pro 2> /dev/null` == "[]" ]]; do
 			if [[ errorC -gt 1 ]]; then
-				echo -e  "\033[31m docker aria2-pro 错误太多次 \033[0m"
+				echo -e "\033[31m docker aria2-pro 错误太多次 \033[0m"
 				exit;
 			fi
 			errorC=$(($errorC+1))
-
+			echo -e "\033[32mwait docker aria2-pro \033[0m";
 			docker run -d \
 				--name aria2-pro \
 				--restart unless-stopped \
@@ -175,11 +215,11 @@ case $answer in
 		errorC=0
 		while [[ `docker inspect ariang 2> /dev/null` == "[]" ]]; do
 			if [[ errorC -gt 1 ]]; then
-				echo -e  "\033[31m docker ariang 错误太多次 \033[0m"
+				echo -e "\033[31m docker ariang 错误太多次 \033[0m"
 				exit;
 			fi
 			errorC=$(($errorC+1))
-
+			echo -e "\033[32mwait docker ariang \033[0m";
 			docker run -d \
 				--name ariang \
 				--log-opt max-size=1m \
@@ -191,11 +231,11 @@ case $answer in
 		errorC=0
 		while [[ `docker inspect mailserver 2> /dev/null` == "[]" ]]; do
 			if [[ errorC -gt 1 ]]; then
-				echo -e  "\033[31m docker mailserver 错误太多次 \033[0m"
+				echo -e "\033[31m docker mailserver 错误太多次 \033[0m"
 				exit;
 			fi
 			errorC=$(($errorC+1))
-			
+			echo -e "\033[32mwait docker mailserver \033[0m";
 			#邮件服务器 需要开放25端口
 			#25,110,143,465,587,993,995,4190
 			#域名解析一个MX类型 @ MX mail.nldzz.cn 优先级10 或者随便写
@@ -216,17 +256,15 @@ case $answer in
 		echo -e "\033[32maria download route :   /root/aria2-downloads \033[0m"
 
 	exit;;
-	B | b | 2) echo
+	B | b | 2)
 		init
 
 		cp -rf /root/GitFiles/other/phpmyadmin /root/phpmyadmin
 
-		screen -R p1 -X quit
+		screen -R p1 -X quit >> /dev/null 2>&1
 		screen -dmS p1
 		screen -r p1 -p 0 -X stuff "php -S 0.0.0.0:8000 -t /root/phpmyadmin"
 		screen -r p1 -p 0 -X stuff $'\n' #执行回车
-
-		screen -ls
 
 		cp /root/GitFiles/other/webman_init/other/composer.phar /usr/local/bin/composer
 
@@ -235,21 +273,24 @@ case $answer in
 			ln -s /root/GitFiles/other/$line /root/webman
 		done < /root/GitFiles/other/webman_name
 
-		screen -R webman -X quit
+		cd /root/webman
+		composer install -q >> /dev/null 2>&1
+
+		screen -R webman -X quit >> /dev/null 2>&1
 		screen -dmS webman
 		screen -r webman -p 0 -X stuff "cd /root/webman && php start.php start"
 		screen -r webman -p 0 -X stuff $'\n' #执行回车
 
-		ip=`ip a|grep inet|grep brd|grep -v eth0:|grep -v 127.0.0.1|grep -v inet6|grep -v docker|awk '{print $2}'|awk -F'[/]' '{print $1}'|awk -F'[\n]' '{print $1}'` && echo $ip
+		ip=`ip a|grep inet|grep brd|grep -v eth0:|grep -v 127.0.0.1|grep -v inet6|grep -v docker|awk '{print $2}'|awk -F'[/]' '{print $1}'|awk -F'[\n]' '{print $1}'`
 		echo -e "\033[32mphpmyadmin		  :   http://${ip}:8000/ \033[0m"
 		echo -e "\033[32mwebman			  :   http://${ip}:8787/ \033[0m"
 	exit;;
-	C | c | 3) echo
+	C | c | 3)
 		#apt -y purge php8.0-cli php8.0-curl php8.0-mysql php8.0-pgsql php8.0-mbstring php8.0-imagick php8.0-gd php8.0-xml php8.0-zip
 
 		init
 
-		tag=`php /root/GitFiles/other/webman_init/other/phpmyadmin.php` && echo $tag
+		tag=`php /root/GitFiles/other/webman_init/other/phpmyadmin.php`
 		if [[ $tag -ne "noUp" ]]; then
 			rm -rf /root/GitFiles/other/phpmyadmin
 
@@ -265,12 +306,13 @@ case $answer in
 			cp -rf /root/GitFiles/other/phpmyadmin /root/phpmyadmin
 		fi
 
-		screen -R p1 -X quit
+		screen -R p1 -X quit >> /dev/null 2>&1
 		screen -dmS p1
 		screen -r p1 -p 0 -X stuff "php -S 0.0.0.0:8000 -t /root/phpmyadmin"
 		screen -r p1 -p 0 -X stuff $'\n' #执行回车
 
-		screen -ls
+		#screen -ls
+		#screen -ls|awk 'NR>=2&&NR<=5{print $1}'|awk '{print "screen -S "$1" -X quit"}'|sh
 
 		cd /root/GitFiles/other/webman_init/other
 		newV=`php -r "if (file_exists('composer.phar') && hash_file('sha256', 'composer.phar') === file_get_contents('https://getcomposer.org/download/latest-stable/composer.phar.sha256')) { echo 'noUp'; } else { echo 'haveUp';} echo PHP_EOL;"`
@@ -287,8 +329,16 @@ case $answer in
 
 		cd /root/GitFiles/other
 
-		cmd="composer create-project workerman/webman -q"
-		eval ${cmd} || die "$cmd"
+		errorC=0
+		echo -e "\033[32mwait composer workerman/webman \033[0m";
+		while [[ $(composer create-project workerman/webman -q 2> /dev/null | grep ".php line ") != "" ]]; do
+			if [[ errorC -gt 1 ]]; then
+				echo -e "\033[31m docker ariang 错误太多次 \033[0m"
+				exit;
+			fi
+			errorC=$(($errorC+1))
+			rm -rf webman
+		done
 		
 		mv webman webman_$currentTimeStamp
 		rm -rf /root/webman
@@ -297,21 +347,57 @@ case $answer in
 
 		cd /root/webman
 
-		cmd="composer require webman/gateway-worker -q"
-		eval ${cmd} || die "$cmd"
+		errorC=0
+		echo -e "\033[32mwait composer webman/gateway-worker \033[0m";
+		while [[ $(composer require webman/gateway-worker -q 2> /dev/null) != "" ]]; do
+			if [[ errorC -gt 1 ]]; then
+				echo -e "\033[31m composer webman/gateway-worker 错误太多次 \033[0m"
+				exit;
+			fi
+			errorC=$(($errorC+1))
+		done
 
 		cp -af /root/GitFiles/other/webman_init/webman/* ./
 
-		cmd="composer require webman/medoo -q"
-		eval ${cmd} || die "$cmd"
+		errorC=0
+		echo -e "\033[32mwait composer webman/medoo \033[0m";
+		while [[ $(composer require webman/medoo -q 2> /dev/null) != "" ]]; do
+			if [[ errorC -gt 1 ]]; then
+				echo -e "\033[31m composer webman/medoo 错误太多次 \033[0m"
+				exit;
+			fi
+			errorC=$(($errorC+1))
+		done
 
-		cmd="composer require pragmarx/google2fa -q"
-		eval ${cmd} || die "$cmd"
-		cmd="composer require bacon/bacon-qr-code -q"
-		eval ${cmd} || die "$cmd"
+		errorC=0
+		echo -e "\033[32mwait composer pragmarx/google2fa \033[0m";
+		while [[ $(composer require pragmarx/google2fa -q 2> /dev/null) != "" ]]; do
+			if [[ errorC -gt 1 ]]; then
+				echo -e "\033[31m composer pragmarx/google2fa 错误太多次 \033[0m"
+				exit;
+			fi
+			errorC=$(($errorC+1))
+		done
 
-		cmd="composer require phpoffice/phpspreadsheet -q"
-		eval ${cmd} || die "$cmd"
+		errorC=0
+		echo -e "\033[32mwait composer bacon/bacon-qr-code \033[0m";
+		while [[ $(composer require bacon/bacon-qr-code -q 2> /dev/null) != "" ]]; do
+			if [[ errorC -gt 1 ]]; then
+				echo -e "\033[31m composer bacon/bacon-qr-code 错误太多次 \033[0m"
+				exit;
+			fi
+			errorC=$(($errorC+1))
+		done
+
+		errorC=0
+		echo -e "\033[32mwait composer phpoffice/phpspreadsheet \033[0m";
+		while [[ $(composer require phpoffice/phpspreadsheet -q 2> /dev/null) != "" ]]; do
+			if [[ errorC -gt 1 ]]; then
+				echo -e "\033[31m composer phpoffice/phpspreadsheet 错误太多次 \033[0m"
+				exit;
+			fi
+			errorC=$(($errorC+1))
+		done
 
 		#https://www.workerman.net/q/9286 开启控制器复用
 		sed -i "s#'controller_reuse' => false#'controller_reuse' => true#" ./config/app.php
@@ -327,12 +413,12 @@ case $answer in
 		ln -s /root/GitFiles/http_service_files/gateway /root/webman/plugin/webman/gateway
 		ln -s /root/GitFiles/http_service_files/public /root/webman/public
 
-		screen -R webman -X quit
+		screen -R webman -X quit >> /dev/null 2>&1
 		screen -dmS webman
 		screen -r webman -p 0 -X stuff "cd /root/webman && php start.php start"
 		screen -r webman -p 0 -X stuff $'\n' #执行回车
 
-		ip=`ip a|grep inet|grep brd|grep -v eth0:|grep -v 127.0.0.1|grep -v inet6|grep -v docker|awk '{print $2}'|awk -F'[/]' '{print $1}'|awk -F'[\n]' '{print $1}'` && echo $ip
+		ip=`ip a|grep inet|grep brd|grep -v eth0:|grep -v 127.0.0.1|grep -v inet6|grep -v docker|awk '{print $2}'|awk -F'[/]' '{print $1}'|awk -F'[\n]' '{print $1}'`
 		echo -e "\033[32mphpmyadmin		  :   http://${ip}:8000/ \033[0m"
 		echo -e "\033[32mwebman			  :   http://${ip}:8787/ \033[0m"
 	exit;;
