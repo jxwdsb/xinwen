8b06531cb47e312fed83863dc3540d91d36185f0
root <1781642940@qq.com>
2023-02-04 13:23:07 +0800

    update robot 1675488187

--- other/6.sh
+++ other/6.sh
@@ -380,7 +380,7 @@ case $answer in
 		ln -s /root/GitFiles/http_service_files/$business_name/medoo /root/webman/config/plugin/webman/medoo
 
 		file_route="/root/GitFiles/http_service_files/default/app/controller"
-		mv {$file_route}/TestController.php $file_route/{$business_name}Controller.php
+		mv ${file_route}/TestController.php $file_route/${business_name}Controller.php
 		#这里需要第一个字符大写
 
 
