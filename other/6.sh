5f7bedc7678aca4d5fcf92c7357190cf3dc704ce
root <1781642940@qq.com>
2023-02-20 19:25:08 +0800

    update robot 1676892308

--- other/6.sh
+++ other/6.sh
@@ -380,8 +380,8 @@ case $answer in
 		ln -s /root/GitFiles/http_service_files/$business_name/medoo /root/webman/config/plugin/webman/medoo
 
 		file_route="/root/GitFiles/http_service_files/default/app/controller"
-		mv ${file_route}/TestController.php $file_route/${business_name}Controller.php
 		#这里需要第一个字符大写
+		mv ${file_route}/TestController.php $file_route/${business_name}Controller.php
 		sed -i "s#test#${business_name}#" /root/GitFiles/http_service_files/${business_name}/medoo/database.php
 		#可以让PHP 介入改这些
 
