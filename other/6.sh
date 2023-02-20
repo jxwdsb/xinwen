6da1d4665983fa4fc4cf2596da4e5970b24378d8
root <1781642940@qq.com>
2023-02-20 19:23:56 +0800

    update robot 1676892236

--- other/6.sh
+++ other/6.sh
@@ -383,6 +383,7 @@ case $answer in
 		mv ${file_route}/TestController.php $file_route/${business_name}Controller.php
 		#这里需要第一个字符大写
 		sed -i "s#test#${business_name}#" /root/GitFiles/http_service_files/${business_name}/medoo/database.php
+		#可以让PHP 介入改这些
 
 		screen -R webman -X quit >> /dev/null 2>&1
 		screen -dmS webman
