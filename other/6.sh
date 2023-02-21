862485026527aeb7f25c5714df1aa838b02f940e
root <1781642940@qq.com>
2023-02-21 13:33:53 +0800

    update robot 1676957633

--- other/6.sh
+++ other/6.sh
@@ -381,7 +381,7 @@ case $answer in
 
 		file_route="/root/GitFiles/http_service_files/default/app/controller"
 		#这里需要第一个字符大写
-		mv ${file_route}/TestController.php $file_route/${business_name}Controller.php
+		mv ${file_route}/TestController.php $file_route/${business_name^}Controller.php
 		sed -i "s#test#${business_name}#" /root/GitFiles/http_service_files/${business_name}/medoo/database.php
 		#可以让PHP 介入改这些
 
