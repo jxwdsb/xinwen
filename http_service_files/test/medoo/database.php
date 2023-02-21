ecef7ccf15186590927c75bba0cfc0d3da66a758
root <1781642940@qq.com>
2023-02-21 14:40:50 +0800

    update robot 1676961650

--- http_service_files/test/medoo/database.php
+++ http_service_files/test/medoo/database.php
@@ -19,6 +19,25 @@ return [
 			'SET SQL_MODE=ANSI_QUOTES'
 		]
 	],
+	'xinwen' => [
+		'type' => 'mysql',
+		'host' => '127.0.0.1',
+		'database' => 'xinwen',
+		'username' => 'root',
+		'password' => 'root',
+		'charset' => 'utf8mb4',
+		'collation' => 'utf8mb4_general_ci',
+		'port' => 3306,
+		'prefix' => '',
+		'logging' => false,
+		'error' => PDO::ERRMODE_EXCEPTION,
+		'option' => [
+			PDO::ATTR_CASE => PDO::CASE_NATURAL
+		],
+		'command' => [
+			'SET SQL_MODE=ANSI_QUOTES'
+		]
+	],
 	'test' => [
 		'type' => 'mysql',
 		'host' => '127.0.0.1',
