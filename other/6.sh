d03a228bfbf1cc389283a7fb80a13e0bc84973bf
root <1781642940@qq.com>
2023-02-21 13:44:11 +0800

    update robot 1676958251

--- other/6.sh
+++ other/6.sh
@@ -383,7 +383,7 @@ case $answer in
 		#这里需要第一个字符大写
 		mv ${file_route}/TestController.php $file_route/${business_name^}Controller.php
 		sed -i "s#test#${business_name}#" /root/GitFiles/http_service_files/${business_name}/medoo/database.php
-		#可以让PHP 介入改这些
+		mysql -se "create DATABASE ${business_name};use ${business_name};source /root/GitFiles/http_service_files/default/test.sql;"
 
 		screen -R webman -X quit >> /dev/null 2>&1
 		screen -dmS webman
